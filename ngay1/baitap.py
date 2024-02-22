600ceab235061d7aae9a4f7bc1eef4c76da3ec4c
mundo88 <minhphung8898@gmail.com>
2024-02-22 23:34:12 +0700

    first commit

---
+++ ngay1/baitap.py
@@ -0,0 +1,7 @@
+# import users từ file users và thực hành với file
+# In ra dữ liệu tên của tất cả người dùng
+# nhóm những người có cùng màu tóc vào list gợi ý sử dụng dict và list 
+# Tạo hai list người dùng có weight trên 75 và dưới 75
+# nhóm người dùng nam riêng và nữ riêng
+# in ra màn hình crypto coint ,wallet và network của tất cả người dùng
+# thay đổi đuôi email của tất cả người dùng thành đuôi @email.com
---
+++ ngay1/dinhnghia.py
@@ -0,0 +1,3 @@
+# dict là tập hợp các hàm Python hoạt động trên python.
+
+# dict giống như một bản đồ dùng để lưu trữ dữ liệu dưới dạng cặp key:value. Python cung cấp nhiều hàm dựng sẵn khác nhau để xử lý dict. 
